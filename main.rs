831a4b1b1e3268a019aeb691e411c615146fcd16
Mikko Perttunen <cyndis@kapsi.fi>
2013-01-17 19:32:49 +0200

    Change scene in main.rs.

--- main.rs
+++ main.rs
@@ -8,46 +8,45 @@ use io::WriterUtil;
 
 fn main() {
   let mut scene = scene::LinearScene::new();
-/*
-  let sphere = scene::Object {
-    shape: scene::Sphere({origin: lmath::vec::Vec3 { x: 0.0, y: 0.0, z: -5.0 }, radius: 1.0}),
+
+  let sky = scene::Object {
+    shape: scene::AABB({min: lmath::vec::Vec3 { x: -10.0, y: 6.0, z: -10.0 },
+                        max: lmath::vec::Vec3 { x:  10.0, y: 6.1, z:   0.0 }}),
+    emits: true,
+    color: scene::image::RGB { r: 1.0, g: 1.0, b: 1.0 },
+    rotation: lmath::quat::Quat::identity()
+  };
+  scene.add(sky);
+
+  let floor = scene::Object {
+    shape: scene::AABB({min: lmath::vec::Vec3 { x: -10.0, y: -0.1, z: -10.0 },
+                        max: lmath::vec::Vec3 { x:  10.0, y:  0.0, z:   0.0 }}),
     emits: false,
-    color: scene::image::RGB { r: 0.8, g: 0.2, b: 0.2 },
+    color: scene::image::RGB { r: 0.6, g: 0.6, b: 0.6 },
     rotation: lmath::quat::Quat::identity()
   };
-  scene.add(sphere);
+  scene.add(floor);
 
-  let light = scene::Object {
-    shape: scene::Sphere({origin: lmath::vec::Vec3 { x: 2.0, y: 0.0, z: -3.0 }, radius: 0.6}),
-    emits: true,
-    color: scene::image::RGB { r: 1.0, g: 1.0, b: 1.0 },
-    rotation:lmath::quat::Quat::identity()
-  };
-  scene.add(light);
-*/
-  let plane = scene::Object {
-    shape: scene::AABB({min: lmath::vec::Vec3 { x: -5.0, y: -5.0, z: 1.0 },
-                        max: lmath::vec::Vec3 { x:  5.0, y:  5.0, z: 1.0 }}),
-    emits: true,
-    color: scene::image::RGB { r: 1.0, g: 1.0, b: 1.0 },
+  let ball = scene::Object {
+    shape: scene::Sphere({origin: lmath::vec::Vec3 { x: -2.0, y: 1.0, z: -4.0 },
+                          radius: 1.0}),
+    emits: false,
+    color: scene::image::RGB { r: 0.8, g: 0.3, b: 0.3 },
     rotation: lmath::quat::Quat::identity()
   };
-  scene.add(plane);
+  scene.add(ball);
 
-  let rad = numeric::types::angle::Radians(3.14/4.0);
-  let quat = lmath::quat::Quat::from_angle_axis(rad, &lmath::vec::Vec3 { x: 0.3, y: 0.3, z: 0.0 });
-  
   let box = scene::Object {
-    shape: scene::AABB({min: lmath::vec::Vec3 { x: -0.5, y: -0.5, z: -4.0 },
-                        max: lmath::vec::Vec3 { x:  0.5, y:  0.5, z: -3.5 }}),
+    shape: scene::AABB({min: lmath::vec::Vec3 { x: 1.0, y: 0.0, z: -7.0 },
+                        max: lmath::vec::Vec3 { x: 2.0, y: 1.0, z: -6.0 }}),
     emits: false,
-    color: scene::image::RGB { r: 0.0, g: 0.2, b: 1.0 },
-    rotation: quat
+    color: scene::image::RGB { r: 0.3, g: 0.8, b: 0.8 },
+    rotation: lmath::quat::Quat::from_angle_axis(numeric::types::angle::Degrees(45.0), &lmath::vec::Vec3 { x: 0.0, y: 1.0, z: 0.0 })
   };
   scene.add(box);
 
-  let camera = scene::Camera::new(1.57, 1.33, lmath::vec::Vec3 { x: 0.0, y: 0.0, z: 0.0 },
-                                  lmath::vec::Vec3 { x: 0.0, y: 0.0, z: -1.0 });
+  let camera = scene::Camera::new(1.57, 1.33, lmath::vec::Vec3 { x: 0.0, y: 2.0, z: 0.0 },
+                                  lmath::vec::Vec3 { x: 0.0, y: 0.0, z: -5.0 });
 
   let mut result = scene::image::Image::new(800, 600);
   
@@ -56,7 +55,7 @@ fn main() {
   let mut futures = ~[];
   let ca = std::arc::ARC(camera);
   let sa = std::arc::ARC(scene);
-  for uint::range(0,30) |_| {
+  for uint::range(0,150) |_| {
     let ca = ca.clone(); let sa = sa.clone();
     futures.push(do std::future::spawn |move ca, move sa| {
       scene::render_singlethread(std::arc::get(&ca), std::arc::get(&sa), 800, 600)
